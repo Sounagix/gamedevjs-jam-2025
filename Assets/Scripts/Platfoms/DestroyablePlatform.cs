50a30f9f21aee1abff325eb07f3527e8db4100de
Amaro <amaro.blest@gmail.com>
2025-04-16 20:18:30 +0200

    ADDED: destroyable platform

---
+++ Assets/Scripts/Platfoms/DestroyablePlatform.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyablePlatform : Platform
+{
+    [SerializeField]
+    private float _destroyDelay;
+
+    private Coroutine _destroyCoroutine;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(PlayerActions.PlayerTag))
+        {
+            _destroyCoroutine = StartCoroutine(DestroyPlatform());
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(PlayerActions.PlayerTag) && _destroyCoroutine != null)
+        {
+            StopCoroutine(_destroyCoroutine);
+        }
+    }
+
+    private IEnumerator DestroyPlatform()
+    {
+        yield return new WaitForSeconds(_destroyDelay);
+        Destroy(gameObject);
+    }
+}
